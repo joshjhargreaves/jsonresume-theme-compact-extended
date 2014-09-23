74c4059c079f8b50f7f01aaae8206d5671e6afe4
Josh Hargreaves <joshyhargreaves@gmail.com>
2014-09-23 13:02:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var Handlebars = require('handlebars');
+var Moment = require('moment');
+
+function render(resume) {
+  var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+
+	return Handlebars.compile(template)({
+		css: css,
+		resume: resume
+	});
+};
+
+
+Handlebars.registerHelper("prettifyDate", function(resumeDate) {
+  if (!resumeDate) {
+    return 'Present';
+  }
+  var newDate = Moment(resumeDate).format('MMM YYYY');
+  console.log('newDate: ' + newDate);
+  return newDate;
+});
+
+Handlebars.registerHelper('toLowerCase', function(str) {
+  return str.toLowerCase();
+});
+
+Handlebars.registerHelper('unless_blank', function(item, block) {
+  return (item && item.replace(/\s/g,"").length) ? block.fn(this) : block.inverse(this);
+});
+
+module.exports = {
+	render: render
+}
